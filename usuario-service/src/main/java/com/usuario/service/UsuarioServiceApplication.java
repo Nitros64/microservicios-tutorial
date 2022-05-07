5a340a6d07519ca679234c5d77b5f6901958ea32
Nitros64 <nitros.n64@gmail.com>
2022-05-07 10:03:04 -0400

    Rest Template - feign client

---
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -0,0 +1,15 @@
+package com.usuario.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients //habilitar la comunicacion con OpenFeign
+@SpringBootApplication
+public class UsuarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServiceApplication.class, args);
+	}
+
+}
